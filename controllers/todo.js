c1d8825d931a8173bbdac3b7ed220e65202dc748
BryanG <BryanG@Bryans-MacBook-Pro.local>
2019-10-18 16:18:19 -0500

    made index page

--- controllers/todo.js
+++ controllers/todo.js
@@ -3,11 +3,11 @@ const router = express.Router()
 const Todo = require(`../models/todo.js`)
 
 //routes
-
+// new route
 router.get(`/new`, (req, res) => {
 		res.render(`new.ejs`)
 })
-
+// create route
 router.post(`/`, (req, res) => {
 	Todo.create(req.body, (err, todo) => {
 		if (err) {
@@ -15,12 +15,28 @@ router.post(`/`, (req, res) => {
 		} 
 		else {
 			console.log(`this is the create route working`);
-			res.redirect(`/new`)
+			res.redirect(`/todo`)
 		}
 	})
 })
 
+// index route
 
+router.get(`/`, (req, res) => {
+	Todo.find({}, (err, todos) => {
+		if (err) {
+			res.send(err)
+		} 
+		else {
+
+			res.render(`index.ejs`, {
+				todos: todos
+			})
+		}
+	})
+
+	
+})
 
 
 
